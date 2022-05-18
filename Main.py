7b616b0dea298f37fec97ea3b373d7e610573759
K-218 <karimahabchan@gmail.com>
2022-05-18 21:04:15 +0200

    Update Main.py

--- Main.py
+++ Main.py
@@ -24,8 +24,6 @@ def guess_age():
         rem7 = int(input())
         age = (rem3 * 70 + rem5 * 21 + rem7 * 15) % 105
 
-
-
     print("Your age is " + str(age) + "; that's a good time to start programming!")
 
 
